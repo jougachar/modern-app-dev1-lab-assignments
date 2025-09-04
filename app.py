04f09695fbc51f7910456d501f22624e2a23b15b
jougachar <jougachar05@gmail.com>
2025-09-04 22:48:50 +0530

    added csv parsing and dictionary update helper

--- app.py
+++ app.py
@@ -5,4 +5,18 @@ students = {}
 courses = {}
 
 def dict_update(dictionary_name, primary_id, candidate_id, marks):
-    pass
+    if primary_id in dictionary_name.keys():
+        dictionary_name[primary_id][candidate_id] = marks
+    else:
+        dictionary_name[primary_id] = {}
+        dictionary_name[primary_id][candidate_id] = marks
+# Read CSV into Dictionary
+with open('data.csv', mode='r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    line_count = 0
+    for row in csv_reader:
+        dict_update(courses, int(row[' Course id']),
+                    int(row['Student id']), int(row[' Marks']))
+        dict_update(students, int(row['Student id']),
+                    int(row[' Course id']), int(row[' Marks']))
+        line_count += 1
